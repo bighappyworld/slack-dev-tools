d9fb3ce7b271dff91bdfe20bf0cc6a6355e5762d
bighappyworld <bighappyworld@gmail.com>
2014-11-07 10:23:24 +0700

    used the new post file

--- services/xteam.js
+++ services/xteam.js
@@ -3,56 +3,37 @@ module.exports = {
   method: 'POST',
   path: '/slack/xteam/{team}/{service}/{token}',
   handler: function (request, reply) {
-      var querystring = require('querystring');
-      var https = require('https');
-
       var app = request.server.settings.app;
 
       var text = request.payload.text;
+
       if( request.payload.trigger_word )
       {
           text = request.payload.text.replace(request.payload.trigger_word, '').trim();
       }
-
 
       var data = {
           "username": request.payload.user_name,
           "text": text
       };
 
-      var data_json = JSON.stringify(data);
-
-      // An object of options to indicate where to post to
-      var post_options = {
-          host: app.xteam.host,
-          port: '443',
-          path: app.xteam.path + '/' + request.params.team + '/' + request.params.service + '/' + request.params.token ,
-          method: 'POST',
-          headers: {
-              'Content-Type': 'application/json',
-              'Content-Length': data_json.length
-          }
-      };
-
-      // Set up the request
-      var post_req = https.request(post_options, function(res) {
-          res.setEncoding('utf8');
-          var success = false;
-          res.on('data', function (chunk) {
-              success = true;
-          });
-          res.on('end', function() {
-              reply( { "text" : "", "success" : success } );
-          });
-      });
-      post_req.on('error', function(e) {
-          reply( { "text" : app.messages.message_failed, "success" : false } );
-      });
-
-      // post the data
-      post_req.write(data_json);
-      post_req.end();
-
+      var https = require( '../lib/https.js' );
+      try {
+          https.postJSON( data,
+                          app.xteam.host,
+                          '443',
+                          app.xteam.path + '/' + request.params.team + '/' + request.params.service + '/' + request.params.token,
+                          function( err, res ){
+                              if( err ) reply( { "text" : app.messages.message_failed,
+                                                 "success" : false } );
+                              else{
+                                 reply( { "text" : "", "success" : res.response.length > 0 } );
+                              }
+                          });
+      } catch( ex ) {
+        console.debug( ex );
+        reply( { "text" : ex, "success" : false } );
+      }
   },
   config : {
       validate : {
